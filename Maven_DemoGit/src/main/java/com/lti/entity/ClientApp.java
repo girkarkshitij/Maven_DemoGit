da997ec204a854a4e9a7b7c2f3f62ac462daa2c1
Kshitij Girkar <43858028+girkarkshitij@users.noreply.github.com>
2022-07-14 12:51:57 +0530

    Create ClientApp.java

---
+++ Maven_DemoGit/src/main/java/com/lti/entity/ClientApp.java
@@ -0,0 +1,10 @@
+package com.lti.entity;
+
+public class ClientApp {
+	public static void main(String[] args) {
+		Calculator c = new Calculator();
+		int result = c.add(2, 5);
+		
+		System.out.println(result);
+	}
+}
